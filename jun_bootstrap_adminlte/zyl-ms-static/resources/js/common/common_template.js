6acb055f2ee852f06f2f25bcb77cb55ebbbb5f9e
wujun728 <wujun728@hotmail.com>
2021-07-08 21:49:06 +0800

    1

--- jun_bootstrap_adminlte/zyl-ms-static/resources/js/common/common_template.js
+++
@@ -1,7 +0,0 @@
-// 分页模板
-var _template_pages_html = '<%if(pageNo<=1){%><li class="disabled"><a href="javascript:void(0);">«</a></li>'+
-	'<%}else{%><li class=""><a href="javascript:fun_to_page(<%=pageNo-1%>);">«</a></li><%}%>'+
-	'<%if(pageNo>5){%><li class=""><a href="javascript:fun_to_page(1);">1</a></li><li class="disabled"><a href="javascript:void(0);">...</a></li><%}%>'+
-	'<%for(var i=0;i<pageCount;i++){if(i>pageNo+3||i<pageNo-5){continue;}%><li class="<%if((i+1)==pageNo){%>active<%}%>"><a href="javascript:fun_to_page(<%=i+1%>);"><%=i+1%></a></li><%}%>'+
-	'<%if(pageCount-pageNo>5){%><li class="disabled"><a href="javascript:void(0);">...</a></li><li class=""><a href="javascript:fun_to_page(<%=pageCount%>);"><%=pageCount%></a></li><%}%>'+
-	'<%if(pageNo>=pageCount){%><li class="disabled"><a href="javascript:void(0);">»</a></li><%}else{%><li class=""><a href="javascript:fun_to_page(<%=pageNo+1%>);">»</a></li><%}%>';
--- jun_bootstrap_adminlte/zyl-ms/src/main/java/org/zyl/ms/general/GeneralSystemConstant.java
+++
@@ -1,6 +0,0 @@
-package org.zyl.ms.general;
-
-public class GeneralSystemConstant {
-	public static String[] DEFAULT_IMAGES = {"default_user_head.png","default_user_head.png","default_user_head.png","default_user_head.png","default_user_head.png","default_user_head.png","default_user_head.png","default_user_head.png","default_user_head.png","default_user_head.png"};
-	public static String KEY_USER_INFO = "user_info:%s";
-}
